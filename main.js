1b2ea38947bcf8b711eb634e95e405ffa5e8cf35
Li Chen <cxli@email.unc.edu>
2017-08-02 23:20:54 -0400

    updated

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-var CastMember = require("./BasicCard.js");
- var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
+/
+var BasicCard = require('./basic-flashcard.js');
+
+var ClozeCard = require('./cloze-flashcard.js');
+//  command line
+var inquirer = require('inquirer');
+// require fs
+var fs = require('fs');
+
